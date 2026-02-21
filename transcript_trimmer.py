9924c8b0d7a6db44edfe80ba8f2c9263692e64d1
Barbara Shaurette <barbara@bloodredrose.com>
2026-02-21 13:42:58 -0800

    transcription based file trimmer

---
+++ transcript_trimmer.py
@@ -0,0 +1,57 @@
+# /// script
+# requires-python="==3.9"
+# dependencies = [
+#   "numpy==1.26.4",
+#   "openai-whisper==20250625",
+#   "pydub==0.25.1",
+#   "setuptools-rust==1.12.0",
+# ]
+# ///
+
+from pydub import AudioSegment
+import whisper
+
+import os
+import ssl
+import sys
+ssl._create_default_https_context = ssl._create_unverified_context
+
+podcast_path  = sys.argv[1]
+new_podcast_name = podcast_path.split('/')[-1].split('.')[0] + '_clean.mp3'
+
+root_list = podcast_path.split('/')
+del root_list[-1]
+root_path = ('/').join(root_list)
+new_podcast_path = os.path.join(root_path, new_podcast_name)
+
+model = whisper.load_model("base")
+result = model.transcribe(podcast_path)
+
+all_segments = []
+
+for segment in result['segments']:
+    print(f"{segment['id']}: {segment['text']}")
+    all_segments.append({'id': segment['id'], 'start': segment['start'], 'end': segment['end']})
+
+segment_ids_to_remove = []
+response = input("List segment ids to remove (e.g. 35-118, 200, 235, 281-299): ")
+for r in response.split(', '):
+    rspl = r.split('-')
+    if len(rspl) > 1:
+        segment_ids_to_remove.extend(list(range(int(rspl[0]), int(rspl[1]))))
+    else:
+        segment_ids_to_remove.append(int(rspl[0]))
+print(f"Removing segments {segment_ids_to_remove}")
+
+remove_segments = []
+for s in segment_ids_to_remove:
+    remove_segments.append([(d['start'] * 1000, d['end'] * 1000) for d in all_segments if d.get('id') == s][0])
+
+# Remove segments (reverse order so times don't shift)
+audio = AudioSegment.from_mp3(podcast_path)
+for start_ms, end_ms in reversed(remove_segments):
+    audio = audio[:start_ms] + audio[end_ms:]
+audio.export(new_podcast_path, format="mp3")
+
+# uv run transcript_trimmer.py yourfile.mp3
+
