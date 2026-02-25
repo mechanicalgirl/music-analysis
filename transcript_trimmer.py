75e3fbdf82b27275c4406659cf6df7255f76e675
Barbara Shaurette <barbara@bloodredrose.com>
2026-02-25 10:49:01 -0800

    transcript trimmer

--- transcript_trimmer.py
+++ transcript_trimmer.py
@@ -25,7 +25,7 @@ root_path = ('/').join(root_list)
 new_podcast_path = os.path.join(root_path, new_podcast_name)
 
 model = whisper.load_model("base")
-result = model.transcribe(podcast_path)
+result = model.transcribe(podcast_path, fp16=False)
 
 all_segments = []
 
@@ -34,7 +34,7 @@ for segment in result['segments']:
     all_segments.append({'id': segment['id'], 'start': segment['start'], 'end': segment['end']})
 
 segment_ids_to_remove = []
-response = input("List segment ids to remove (e.g. 35-118, 200, 235, 281-299): ")
+response = input("Enter segment ids to remove (e.g. 35-118, 200, 235, 281-299): ")
 if response == 'exit':
     sys.exit()
 for r in response.split(', '):
@@ -55,7 +55,7 @@ for start_ms, end_ms in reversed(x_segments):
     audio = audio[:start_ms] + audio[end_ms:]
 audio.export(new_podcast_path, format="mp3")
 
-new_result = model.transcribe(new_podcast_path)
+new_result = model.transcribe(new_podcast_path, fp16=False)
 for segment in new_result['segments']:
     print(segment['text'])
 
